73fc6bfd3b709d9132a476386702ece7aa06c69a
TiagoFNPaiva <22tiagopaiva22@gmail.com>
2021-02-22 01:30:12 +0000

    make it better

--- apps/js/script.js
+++ apps/js/script.js
@@ -21,12 +21,6 @@ const bCard = document.getElementById("receitas");
 const cCard = document.getElementById("fun");
 const dCard = document.getElementById("roupeiro");
 
-const navWrap = document.getElementsByClassName("bg_wrap")[0];
-const aWrap = document.getElementsByClassName("bg_wrap")[1];
-const bWrap = document.getElementsByClassName("bg_wrap")[2];
-const cWrap = document.getElementsByClassName("bg_wrap")[3];
-const dWrap = document.getElementsByClassName("bg_wrap")[4];
-
 const btn = document.getElementById("scroll");
 const bhome = document.getElementsByClassName("scrollnav")[0];
 const bup = document.getElementsByClassName("scrollup")[0];
@@ -38,16 +32,16 @@ const bdown = document.getElementsByClassName("scrolldown")[0];
 
 const scrollUp = () => {
     const currentP = Math.round(window.scrollY) - 5;
-    if (isInViewport(aWrap) >= currentP) {
+    if (isInViewport(aCard) >= currentP) {
         return;
     }
-    else if (isInViewport(bWrap) >= currentP) {
+    else if (isInViewport(bCard) >= currentP) {
         aCard.scrollIntoView();
     }
-    else if (isInViewport(cWrap) >= currentP) {
+    else if (isInViewport(cCard) >= currentP) {
         bCard.scrollIntoView();
     }
-    else if (isInViewport(dWrap) >= currentP) {
+    else if (isInViewport(dCard) >= currentP) {
         cCard.scrollIntoView();
     }
     else {
@@ -60,33 +54,33 @@ function isInViewport(el) {
 }
 const scrollDown = () => {
     const currentP = Math.round(window.scrollY) + 5;
-    if (isInViewport(dWrap) <= currentP) {
+    if (isInViewport(dCard) <= currentP) {
         return;
     }
-    else if (isInViewport(cWrap) <= currentP) {
+    else if (isInViewport(cCard) <= currentP) {
         dCard.scrollIntoView();
     }
-    else if (isInViewport(bWrap) <= currentP) {
+    else if (isInViewport(bCard) <= currentP) {
         cCard.scrollIntoView();
     }
-    else if (isInViewport(aWrap) <= currentP) {
+    else if (isInViewport(aCard) <= currentP) {
         bCard.scrollIntoView();
     }
-    else if (isInViewport(aWrap) > currentP) {
+    else if (isInViewport(aCard) > currentP) {
         aCard.scrollIntoView();
     }
 }
 
 const hide = () => {
-    if (document.body.scrollTop >= (isInViewport(aWrap) * 0.8) || document.documentElement.scrollTop >= (isInViewport(aWrap)) * 0.8) {
+    if (document.body.scrollTop >= (isInViewport(aCard) * 0.8) || document.documentElement.scrollTop >= (isInViewport(aCard)) * 0.8) {
         btn.style.display = "block";
-        if (document.body.scrollTop >= (isInViewport(bWrap) * 0.6) || document.documentElement.scrollTop >= (isInViewport(bWrap)) * 0.6) {
+        if (document.body.scrollTop >= (isInViewport(aCard) * 1.05) || document.documentElement.scrollTop >= (isInViewport(aCard)) * 1.05) {
             bup.style.visibility = "unset";
         }
         else {
             bup.style.visibility = "hidden";
         }
-        if (document.body.scrollTop >= (isInViewport(dWrap) * 1) || document.documentElement.scrollTop >= (isInViewport(dWrap)) * 1) {
+        if (document.body.scrollTop >= (isInViewport(dCard) * 1) || document.documentElement.scrollTop >= (isInViewport(dCard)) * 1) {
             bdown.style.visibility = "hidden";
         }
         else {
@@ -97,20 +91,3 @@ const hide = () => {
         btn.style.display = "none";
     }
 }
-
-
-/*
-
-const play = () => {
-    const html = document.getElementsByClassName("pa1")[0].innerHTML
-    document.getElementsByClassName("pa1")[0].innerHTML = `<div class="video">
-                                                            <img class="close" src="images/red.png" alt="image">
-                                                            <video autoplay loop controls>
-                                                                <source src="images/example.mp4" type="video/mp4">
-                                                            </video>
-                                                            </div>`
-    document.getElementsByClassName("close")[0].addEventListener("click", function () {
-        document.getElementsByClassName("pa1")[0].innerHTML = html;
-    })
-
-}*/
